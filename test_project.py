6dee9f2bfc1a4d5c5e6cef9182bb1f68a7f12133
Roberto Park <betopark97@gmail.com>
2023-12-05 15:04:52 +0900

    add skeleton for test_project.py

---
+++ test_project.py
@@ -0,0 +1,17 @@
+import pytest
+
+def test_check_place():
+    pass
+
+
+def test_calculate_distance():
+    pass
+
+
+def test_find_farthest_cities():
+    pass
+
+
+def test_make_route():
+    pass
+
